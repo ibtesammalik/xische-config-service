e597a84e408ea7d2bf60471103f26ed1762a257e
ibtesamurrehman <ibtesam85@gmail.com>
2024-09-29 22:38:17 +0500

    first commit

---
+++ src/main/java/com/xische/config/XischeConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package com.xische.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class XischeConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XischeConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xische/config/XischeConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xische.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XischeConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
